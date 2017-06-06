4c9a64bbb1cfd511030ac220bfdd79bf84c8c676
mortie <martid0311@gmail.com>
2017-06-06 20:14:32 +0200

    more precise language

--- example/conf.mk
+++ example/conf.mk
@@ -15,8 +15,8 @@ FLAGS_NDBG=-o3
 WARN=all pedantic extra error
 
 # If we compile with `make ARM=1`, cross compile for arm64.
-# This is usually not necessary unless you specifically want to support ARM
-# out of the box.
+# This is usually not necessary unless you specifically want to support
+# cross compiling for ARM out of the box.
 ifeq ($(ARM),1)
   TOOLCHAIN=aarch64-linux-gnu-
   CC=gcc
